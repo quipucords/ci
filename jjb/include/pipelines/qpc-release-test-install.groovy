003e05c6ac677549e834dc3ebe320911836ce3bf
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2019-02-15 17:02:42 -0500

    got pipeline template working. cleaning it up and making sure all the branches work with it

--- jjb/include/pipelines/qpc-release-test-install.groovy
+++ jjb/include/pipelines/qpc-release-test-install.groovy
@@ -1,6 +1,6 @@
-release_version = "0.0.46"
+// {release}
 
-def setupDocker() {
+def setupDocker() {{
     echo 'Setup docker configuration'
 
     sh '''\
@@ -8,35 +8,42 @@ def setupDocker() {
     echo "DOCKER_CERT_PATH=/etc/docker" >> docker.conf
     sudo cp docker.conf /etc/sysconfig/docker
     '''.stripIndent()
-}
+}}
 
 
-def setupQPC() {
-    sh 'rm quipucords.*.tar.gz'
+def setupQPC() {{
+
+    sh '''\
+    ls -l
+    if [ -f quipucords.*tar.gz ]; then
+        rm quipucords.*.tar.gz
+    fi
+    '''.stripIndent()
+
     echo "load docker container from tarball"
-    sh "curl -k -O -sSL https://github.com/quipucords/quipucords/releases/download/${release_version}/quipucords.${release_version}.install.tar.gz"
+    sh "curl -k -O -sSL https://github.com/quipucords/quipucords/releases/download/{release}/quipucords.{release}.install.tar.gz"
 
-    echo "extract the installer into ${WORKSPACE}/install"
+    echo "extract the installer into ${{WORKSPACE}}/install"
 
-    sh "tar -xvzf ${WORKSPACE}/quipucords.${release_version}.install.tar.gz"
+    sh "tar -xvzf ${{WORKSPACE}}/quipucords.{release}.install.tar.gz"
 
     echo "Execute install.sh to install"
-    dir("${WORKSPACE}/install") {
+    dir("${{WORKSPACE}}/install") {{
         sh 'pwd'
         sh 'ls -l'
-        sh 'sudo ./install.sh -e server_install_dir=${WORKSPACE}'
-    }
-}
+        sh 'sudo ./install.sh -e server_install_dir=${{WORKSPACE}}'
+    }}
+}}
 
 
-def runInstallTests(distro) {
-    if (distro ==~ /f\d\d/) {
+def runInstallTests(distro) {{
+    if (distro ==~ /f\d\d/) {{
         sh 'sudo dnf -y install python-pip'
-    } else {
+    }} else {{
         sh 'sudo yum -y install python-pip'
-    }
+    }}
 
-    if (distro == 'rhel6') {
+    if (distro == 'rhel6') {{
         sh 'sudo pip install pexpect nose'
 
         sh '''\
@@ -44,102 +51,103 @@ def runInstallTests(distro) {
         nosetests --with-xunit --xunit-file=junit.xml ci/scripts/quipucords/master/install/test_install.py
         set -e
         '''.stripIndent()
-    } else {
+    }} else {{
         sh 'sudo pip install -U pip'
         sh 'sudo pip install pexpect pytest'
 
         sh """\
         set +e
-        pytest --junit-prefix ${distro} --junit-xml junit.xml ci/scripts/quipucords/master/install/test_install.py
+        pytest --junit-prefix ${{distro}} --junit-xml junit.xml ci/scripts/quipucords/master/install/test_install.py
         set -e
         """.stripIndent()
-    }
-}
+    }}
+}}
 
 
-stage('Test Install') {
-    parallel 'CentOS 7 Install': {
-        node('centos7-os') {
-            copyArtifacts filter: 'quipucords.*.tar.gz', fingerprintArtifacts: true, projectName: 'qpc-master', selector: upstream(allowUpstreamDependencies: false, fallbackToLastSuccessful: true, upstreamFilterStrategy: 'UseGlobalSetting')
-
-            dir('ci') {
+stage('Test Install') {{
+    parallel 'CentOS 7 Install': {{
+        node('centos7-os') {{
+            dir('ci') {{
                 git 'https://github.com/quipucords/ci.git'
-            }
+            }}
 
-            sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {
-                withCredentials([file(credentialsId: '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {
-                    withEnv(['DISTRO=centos7', 'RELEASE=master']) {
+            sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {{
+                withCredentials([file(credentialsId:
+                '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {{
+                    withEnv(['DISTRO=centos7', 'RELEASE=master']) {{
                         setupDocker()
 
                         setupQPC()
 
                         runInstallTests 'centos7'
-                    }
-                }
-            }
+                    }}
+                }}
+            }}
 
             junit 'junit.xml'
-        }
-    }, 'Fedora 27': {
-        node('f27-os') {
-            copyArtifacts filter: 'quipucords.*.tar.gz', fingerprintArtifacts: true, projectName: 'qpc-master', selector: upstream(allowUpstreamDependencies: false, fallbackToLastSuccessful: true, upstreamFilterStrategy: 'UseGlobalSetting')
+        }}
+    }}, 'Fedora 27': {{
+        node('f27-os') {{
 
-            dir('ci') {
+            dir('ci') {{
                 git 'https://github.com/quipucords/ci.git'
-            }
+            }}
 
-            sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {
-                withCredentials([file(credentialsId: '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {
-                    withEnv(['DISTRO=f27', 'RELEASE=master']) {
+            sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {{
+                withCredentials([file(credentialsId:
+                '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {{
+
+                    withEnv(['DISTRO=f27', 'RELEASE=master']) {{
                         setupDocker()
 
                         setupQPC()
 
                         runInstallTests 'f27'
-                    }
-                }
-            }
+                    }}
+                }}
+            }}
 
             junit 'junit.xml'
-        }
-    }, 'Fedora 28': {
-        node('f28-os') {
-            copyArtifacts filter: 'quipucords.*.tar.gz', fingerprintArtifacts: true, projectName: 'qpc-master', selector: upstream(allowUpstreamDependencies: false, fallbackToLastSuccessful: true, upstreamFilterStrategy: 'UseGlobalSetting')
+        }}
+    }}, 'Fedora 28': {{
+        node('f28-os') {{
 
-            dir('ci') {
+            dir('ci') {{
                 git 'https://github.com/quipucords/ci.git'
-            }
+            }}
 
-            sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {
-                withCredentials([file(credentialsId: '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {
-                    withEnv(['DISTRO=f28', 'RELEASE=master']) {
+            sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {{
+                withCredentials([file(credentialsId:
+                '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {{
+                    withEnv(['DISTRO=f28', 'RELEASE=master']) {{
                         setupDocker()
 
                         setupQPC()
 
                         runInstallTests 'f28'
-                    }
-                }
-            }
+                    }}
+                }}
+            }}
 
             junit 'junit.xml'
-        }
-    }, 'RHEL6 Install': {
-        node('rhel6-os') {
-            copyArtifacts filter: 'quipucords.*.tar.gz', fingerprintArtifacts: true, projectName: 'qpc-master', selector: upstream(allowUpstreamDependencies: false, fallbackToLastSuccessful: true, upstreamFilterStrategy: 'UseGlobalSetting')
+        }}
+    }}, 'RHEL6 Install': {{
+        node('rhel6-os') {{
 
-            dir('ci') {
+            dir('ci') {{
                 git 'https://github.com/quipucords/ci.git'
-            }
+            }}
 
-            configFileProvider([configFile(fileId: '5b276700-674e-4a0f-af91-3e725ed7a311', targetLocation: 'rhel6-custom.repo')]) {
-                sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {
-                    withCredentials([file(credentialsId: '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {
-                        withEnv(['DISTRO=rhel6', 'RELEASE=master']) {
+            configFileProvider([configFile(fileId:
+            '5b276700-674e-4a0f-af91-3e725ed7a311', targetLocation: 'rhel6-custom.repo')]) {{
+                sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {{
+                    withCredentials([file(credentialsId:
+                    '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {{
+                        withEnv(['DISTRO=rhel6', 'RELEASE=master']) {{
                             setupDocker()
 
                             sh '''\
-                            sudo cp ${WORKSPACE}/rhel6-custom.repo /etc/yum.repos.d/rhel6-rcm-internal.repo
+                            sudo cp ${{WORKSPACE}}/rhel6-custom.repo /etc/yum.repos.d/rhel6-rcm-internal.repo
                             sudo yum clean all
                             sudo yum-config-manager --disable base
                             sudo yum-config-manager --disable optional
@@ -148,25 +156,26 @@ stage('Test Install') {
                             setupQPC()
 
                             runInstallTests 'rhel6'
-                        }
-                    }
-                }
-            }
+                        }}
+                    }}
+                }}
+            }}
 
             junit 'junit.xml'
-        }
-    }, 'RHEL7 Install': {
-        node('rhel7-os') {
-            copyArtifacts filter: 'quipucords.*.tar.gz', fingerprintArtifacts: true, projectName: 'qpc-master', selector: upstream(allowUpstreamDependencies: false, fallbackToLastSuccessful: true, upstreamFilterStrategy: 'UseGlobalSetting')
+        }}
+    }}, 'RHEL7 Install': {{
+        node('rhel7-os') {{
 
-            dir('ci') {
+            dir('ci') {{
                 git 'https://github.com/quipucords/ci.git'
-            }
+            }}
 
-            configFileProvider([configFile(fileId: '0f157b1a-7068-4c75-a672-3b1b90f97ddd', targetLocation: 'rhel7-custom.repo')]) {
-                sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {
-                    withCredentials([file(credentialsId: '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {
-                        withEnv(['DISTRO=rhel7', 'RELEASE=master']) {
+            configFileProvider([configFile(fileId:
+            '0f157b1a-7068-4c75-a672-3b1b90f97ddd', targetLocation: 'rhel7-custom.repo')]) {{
+                sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {{
+                    withCredentials([file(credentialsId:
+                    '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {{
+                        withEnv(['DISTRO=rhel7', 'RELEASE=master']) {{
                             setupDocker()
 
                             sh 'sudo cp rhel7-custom.repo /etc/yum.repos.d/rhel7-rcm-internal.repo'
@@ -174,12 +183,12 @@ stage('Test Install') {
                             setupQPC()
 
                             runInstallTests 'rhel7'
-                        }
-                    }
-                }
-            }
+                        }}
+                    }}
+                }}
+            }}
 
             junit 'junit.xml'
-        }
-    }
-}
+        }}
+    }}
+}}
