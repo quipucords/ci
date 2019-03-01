e742c045014a7201f05effd2f4e436eed61d7dd9
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2019-03-01 15:19:32 -0500

    created generic tests pipeline

---
+++ jjb/include/pipelines/qpc-tests-pipeline.groovy
@@ -0,0 +1,262 @@
+// {release} {install_type}
+
+def setupDocker() {{
+    echo 'Setup docker configuration'
+
+    sh '''\
+    echo "OPTIONS=--log-driver=journald" > docker.conf
+    echo "DOCKER_CERT_PATH=/etc/docker" >> docker.conf
+    sudo cp docker.conf /etc/sysconfig/docker
+    '''.stripIndent()
+}}
+
+def installQPC() {{
+    echo "Execute install.sh to install"
+    dir("${{WORKSPACE}}/install") {{
+        sh 'pwd'
+        sh 'ls -l'
+        sh 'sudo ./install.sh -e server_install_dir=${{WORKSPACE}}'
+    }}
+}}
+
+def installQPCNoSupervisorD() {{
+    echo "Execute install.sh to install without supervisord"
+    dir("${{WORKSPACE}}/install") {{
+        sh 'pwd'
+        sh 'ls -l'
+        sh 'sudo ./install.sh -e server_install_dir=${{WORKSPACE}} -e use_supervisord=false'
+
+        // Docker log to check for supervisord
+        sh 'sudo docker ps -a'
+        sh 'sudo docker logs quipucords | grep -i "Running without supervisord"'
+    }}
+}}
+
+def getQuipucords() {{
+    // Grabs the quipucords build
+    echo "Copying Latest build artifact..."
+    copyArtifacts filter: 'quipucords.*.tar.gz', fingerprintArtifacts: true, projectName: 'qpc-testing-build', selector: lastCompleted()
+}}
+
+def getMasterQPC() {{
+    // Grabs the master qpc builds and sets it up
+    sh 'ls -lah'
+    getQuipucords()
+
+    sh 'ls -lah'
+    echo 'Extract the installer script'
+    sh 'tar -xvzf quipucords.0.0.47.install.tar.gz'
+
+    sh 'ls -lah'
+    echo 'Copy container to installer packages directory'
+    sh '''\
+    ls -lah
+    mkdir -p install/packages
+    mv quipucords.0.0.47.tar.gz install/packages/
+    '''.stripIndent()
+
+}}
+
+def getReleasedQPC() {{
+    // Clean up QPC package if here...
+    sh '''\
+    ls -l
+    if [ -f quipucords.*tar.gz ]; then
+        rm quipucords.*.tar.gz
+    fi
+    '''.stripIndent()
+
+    // Pulls down Released QPC Build
+    echo "load docker container from tarball"
+    sh "curl -k -O -sSL https://github.com/quipucords/quipucords/releases/download/{release}/quipucords.{release}.install.tar.gz"
+
+    echo "extract the installer into ${{WORKSPACE}}/install"
+
+    sh "tar -xvzf ${{WORKSPACE}}/quipucords.{release}.install.tar.gz"
+
+}}
+
+def setupQPC() {{
+    // get QPC
+    if ('{release}' == 'master') {{
+        getMasterQPC()
+    }} else {{
+        getReleasedQPC()
+    }}
+
+    // Install QPC
+    if ('{install_type}' == 'nosupervisord') {{
+        installQPCNoSupervisorD()
+    }} else {{
+        installQPC()
+    }}
+
+}}
+
+
+def runInstallTests(distro) {{
+    if (distro ==~ /f\d\d/) {{
+        sh 'sudo dnf -y install python-pip'
+    }} else {{
+        sh 'sudo yum -y install python-pip'
+    }}
+
+    if (distro == 'rhel6') {{
+        sh 'sudo pip install pexpect nose'
+
+        sh '''\
+        set +e
+        nosetests --with-xunit --xunit-file=junit.xml ci/scripts/quipucords/master/install/test_install.py
+        set -e
+        '''.stripIndent()
+    }} else {{
+        sh 'sudo pip install -U pip'
+        sh 'sudo pip install pexpect pytest'
+
+        sh """\
+        set +e
+        pytest --junit-prefix ${{distro}} --junit-xml junit.xml ci/scripts/quipucords/master/install/test_install.py
+        set -e
+        """.stripIndent()
+    }}
+}}
+
+
+def camayoc_tests() {{
+    dir('camayoc') {{
+        git 'https://github.com/quipucords/camayoc.git'
+    }}
+    sh "ls -la"
+
+    sh '''\
+    # create pytest configuration file to ignore the insecure
+    # requests warnings that talking to the server over https
+    # without a good cert cause
+    cat >> pytest.ini << EOF
+    [pytest]
+    filterwarnings = ignore::urllib3.exceptions.InsecureRequestWarning
+    EOF
+    '''.stripIndent()
+
+    sh '''\
+    # Install camayoc and run test suite
+
+    pip install ./camayoc[dev] --user
+
+    set +e
+    py.test -c pytest.ini -v --junit-xml ${{WORKSPACE}}/junit.xml camayoc/camayoc/tests/qpc/
+    set -e
+    # Archive server logs to aid in analyzing test failures
+    #cd ${{WORKSPACE}}
+    #tar -cvzf server-logs.tar.gz ${{WORKSPACE}}/log
+    '''.stripIndent()
+}}
+
+stage('Test Install') {{
+    parallel 'CentOS 7 Install': {{
+        node('centos7-os-old') {{
+            stage('Centos7 Install') {{
+                dir('ci') {{
+                    git 'https://github.com/quipucords/ci.git'
+                }}
+
+                sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {{
+                    withCredentials([file(credentialsId:
+                    '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {{
+                        withEnv(['DISTRO=centos7', 'RELEASE={release}']) {{
+                            setupDocker()
+
+                            setupQPC()
+
+                            runInstallTests 'centos7'
+                        }}
+                    }}
+                }}
+            }}
+            junit 'junit.xml'
+        }}
+    }}, 'Fedora 28': {{
+        node('f28-os-old') {{
+            stage('Fedora 28 Install') {{
+                dir('ci') {{
+                    git 'https://github.com/quipucords/ci.git'
+                }}
+
+                sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {{
+                    withCredentials([file(credentialsId:
+                    '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {{
+                        withEnv(['DISTRO=f28', 'RELEASE={release}']) {{
+                            setupDocker()
+
+                            setupQPC()
+
+                            runInstallTests 'f28'
+                        }}
+                    }}
+                }}
+            }}
+            stage('Fedora 28 Camayoc Tests'){{
+                camayoc_tests()
+            }}
+
+            junit 'junit.xml'
+        }}
+    }}, 'RHEL6 Install': {{
+        node('rhel6-os-old') {{
+            stage('rhel6 Install') {{
+                dir('ci') {{
+                    git 'https://github.com/quipucords/ci.git'
+                }}
+
+                configFileProvider([configFile(fileId:
+                '5b276700-674e-4a0f-af91-3e725ed7a311', targetLocation: 'rhel6-custom.repo')]) {{
+                    sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {{
+                        withCredentials([file(credentialsId:
+                        '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {{
+                            withEnv(['DISTRO=rhel6', 'RELEASE={release}']) {{
+                                setupDocker()
+
+                                sh '''\
+                                sudo cp ${{WORKSPACE}}/rhel6-custom.repo /etc/yum.repos.d/rhel6-rcm-internal.repo
+                                sudo yum clean all
+                                sudo yum-config-manager --disable base
+                                sudo yum-config-manager --disable optional
+                                '''.stripIndent()
+
+                                setupQPC()
+
+                                runInstallTests 'rhel6'
+                            }}
+                        }}
+                    }}
+                }}
+            }}
+            junit 'junit.xml'
+        }}
+    }}, 'RHEL7 Install': {{
+        node('rhel7-os-old') {{
+            stage('rhel7 Install') {{
+                dir('ci') {{
+                    git 'https://github.com/quipucords/ci.git'
+                }}
+
+                configFileProvider([configFile(fileId:
+                '0f157b1a-7068-4c75-a672-3b1b90f97ddd', targetLocation: 'rhel7-custom.repo')]) {{
+                    sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {{
+                        withCredentials([file(credentialsId:
+                        '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {{
+                            withEnv(['DISTRO=rhel7', 'RELEASE={release}']) {{
+                                setupDocker()
+
+                                sh 'sudo cp rhel7-custom.repo /etc/yum.repos.d/rhel7-rcm-internal.repo'
+                                setupQPC()
+                                runInstallTests 'rhel7'
+                            }}
+                        }}
+                    }}
+                }}
+            }}
+            junit 'junit.xml'
+        }}
+    }}
+}}
