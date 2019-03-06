0b230f14183347cfca2c67bdea927d1b94208f37
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2019-03-06 14:54:06 -0500

    Got test stages running, hard coded. Need to clean up.

--- jjb/include/pipelines/qpc-tests-pipeline.groovy
+++ jjb/include/pipelines/qpc-tests-pipeline.groovy
@@ -1,8 +1,13 @@
 // {release} {install_type}
+def qpc_version = "0.0.47"
+def image_name = "quipucords:${{qpc_version}}"
+def tarfile = "quipucords.${{qpc_version}}.tar"
+def targzfile = "${{tarfile}}.gz"
+def install_tar = "quipucords.install.tar"
+def install_targzfile = "${{install_tar}}.gz"
 
 def setupDocker() {{
     echo 'Setup docker configuration'
-
     sh '''\
     echo "OPTIONS=--log-driver=journald" > docker.conf
     echo "DOCKER_CERT_PATH=/etc/docker" >> docker.conf
@@ -10,13 +15,72 @@ def setupDocker() {{
     '''.stripIndent()
 }}
 
-def installQPC() {{
+def startQPCServer = {{
+    sh """\
+    echo 'startQPCServer'
+    pwd
+    ls -lah
+    sudo docker run --name qpc-db -e POSTGRES_PASSWORD=password -d postgres:9.6.10
+    sudo docker run -d -p "9443:443" --link qpc-db:qpc-link \\
+        -e QPC_DBMS_HOST=qpc-db \\
+        -e QPC_DBMS_PASSWORD=password \\
+        -v /tmp:/tmp \
+        -v /home/jenkins/.ssh:/home/jenkins/.ssh \\
+        -v \${{PWD}}/log:/var/log \\
+        -i ${{image_name}}
+    """.stripIndent()
+
+    sh '''\
+    for i in {{1..30}}; do
+        SERVER_ID="$(curl -ks https://localhost:9443/api/v1/status/ | grep server_id || true)"
+
+        if [ "${{SERVER_ID}}" ]; then
+            break
+        fi
+
+        if [ $i -eq 30 ]; then
+            echo "Server took too long to start"
+            exit 1
+        fi
+
+        sleep 1
+    done
+    '''.stripIndent()
+}}
+
+
+def configureDocker = {{
+    sh """\
+    echo "OPTIONS=--log-driver=journald" > docker.conf
+    echo "DOCKER_CERT_PATH=/etc/docker" >> docker.conf
+    echo "INSECURE_REGISTRY=\\"--insecure-registry \${{DOCKER_REGISTRY}}\\"" >> docker.conf
+    sudo cp docker.conf /etc/sysconfig/docker
+    sudo systemctl start docker
+    # TODO: Fix Hard code for now
+    ls -lah
+    pwd
+    ls -lah ..
+    ls -lah install/
+    sudo docker load -i quipucords.0.0.47.tar.gz
+    # make log dir to save server logs
+    mkdir -p log
+    """.stripIndent()
+}}
+
+def installQPCDefault() {{
     echo "Execute install.sh to install"
     dir("${{WORKSPACE}}/install") {{
         sh 'pwd'
         sh 'ls -l'
         sh 'sudo ./install.sh -e server_install_dir=${{WORKSPACE}}'
     }}
+}}
+
+def installQpcClient() {{
+    sh '''\
+    sudo wget -O /etc/yum.repos.d/group_quipucords-qpc-fedora-28.repo https://copr.fedorainfracloud.org/coprs/g/quipucords/qpc/repo/fedora-28/group_quipucords-qpc-fedora28.repo
+    sudo dnf -y install qpc
+    '''.stripIndent()
 }}
 
 def installQPCNoSupervisorD() {{
@@ -36,6 +100,8 @@ def getQuipucords() {{
     // Grabs the quipucords build
     echo "Copying Latest build artifact..."
     copyArtifacts filter: 'quipucords.*.tar.gz', fingerprintArtifacts: true, projectName: 'qpc-testing-build', selector: lastCompleted()
+    copyArtifacts filter: 'quipucords.*.install.tar.gz', fingerprintArtifacts: true, projectName: 'qpc-testing-build', selector: lastCompleted()
+    copyArtifacts filter: 'postgres.*.tar.gz', fingerprintArtifacts: true, projectName: 'qpc-testing-build', selector: lastCompleted()
 }}
 
 def getMasterQPC() {{
@@ -52,7 +118,7 @@ def getMasterQPC() {{
     sh '''\
     ls -lah
     mkdir -p install/packages
-    mv quipucords.0.0.47.tar.gz install/packages/
+    # mv quipucords.0.0.47.tar.gz install/packages/
     '''.stripIndent()
 
 }}
@@ -83,16 +149,16 @@ def setupQPC() {{
     }} else {{
         getReleasedQPC()
     }}
+}}
 
+def installQPC() {{
     // Install QPC
     if ('{install_type}' == 'nosupervisord') {{
         installQPCNoSupervisorD()
     }} else {{
         installQPC()
     }}
-
 }}
-
 
 def runInstallTests(distro) {{
     if (distro ==~ /f\d\d/) {{
@@ -121,6 +187,85 @@ def runInstallTests(distro) {{
     }}
 }}
 
+def setupScanUsers() {{
+    dir('ci') {{
+        git 'https://github.com/quipucords/ci.git'
+    }}
+
+    sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {{
+        withCredentials([file(credentialsId: '50dc19ce-555f-422c-af38-3b5ede422bb4', variable: 'ID_JENKINS_RSA_PUB')]) {{
+            sh 'sudo dnf -y install ansible'
+
+            sh '''\
+            cat > jenkins-slave-hosts <<EOF
+            [jenkins-slave]
+            ${{OPENSTACK_PUBLIC_IP}}
+
+            [jenkins-slave:vars]
+            ansible_user=jenkins
+            ansible_ssh_extra_args=-o StrictHostKeyChecking=no
+            ssh_public_key_file=$(cat ${{ID_JENKINS_RSA_PUB}})
+            EOF
+            '''.stripIndent()
+
+            sh 'ansible-playbook -b -i jenkins-slave-hosts ci/ansible/sonar-setup-scan-users.yaml'
+        }}
+    }}
+}}
+
+
+def setupCamayoc() {{
+    dir('camayoc') {{
+        git 'https://github.com/quipucords/camayoc.git'
+    }}
+
+    sh '''\
+    sudo pip install ./camayoc[dev]
+    cp camayoc/pytest.ini .
+    '''.stripIndent()
+
+    withCredentials([file(credentialsId: '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {{
+        sh '''\
+        mkdir -p /home/jenkins/.ssh
+        cp "${{ID_JENKINS_RSA}}" /home/jenkins/.ssh/id_rsa
+        chmod 0600 /home/jenkins/.ssh/id_rsa
+        '''.stripIndent()
+    }}
+
+    configFileProvider([configFile(fileId: '62cf0ccc-220e-4177-9eab-f39701bff8d7', targetLocation: 'camayoc/config.yaml')]) {{
+        sh '''\
+        sed -i "s/{{jenkins_slave_ip}}/${{OPENSTACK_PUBLIC_IP}}/" camayoc/config.yaml
+        echo 'post-sed'
+        '''.stripIndent()
+
+    }}
+}}
+
+
+def runCamayocTest(testset) {{
+    echo 'Fedora 28: Test '
+    echo testset
+
+
+    sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {{
+        sh '''
+        export xdg_config_home=$pwd
+
+        set +e
+        py.test -c pytest.ini -l -ra -s -vvv --junit-xml api-junit.xml --rootdir camayoc/camayoc/tests/qpc camayoc/camayoc/tests/qpc/api
+        set -e
+
+        sudo docker rm $(sudo docker stop $(sudo docker ps -aq))
+        tar -cvzf test-api-logs.tar.gz log
+        sudo rm -rf log
+        '''.stripIndent()
+    }}
+    echo 'pre archive artifacts'
+    archiveArtifacts 'test-api-logs.tar.gz'
+
+    echo 'pre api-junit'
+    junit 'api-junit.xml'
+}}
 
 def camayoc_tests() {{
     dir('camayoc') {{
@@ -164,7 +309,7 @@ stage('Test Install') {{
                     withCredentials([file(credentialsId:
                     '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {{
                         withEnv(['DISTRO=centos7', 'RELEASE={release}']) {{
-                            setupDocker()
+//                            setupDocker()
 
                             setupQPC()
 
@@ -176,7 +321,7 @@ stage('Test Install') {{
             junit 'junit.xml'
         }}
     }}, 'Fedora 28': {{
-        node('f28-os-old') {{
+        node('f28-os') {{
             stage('Fedora 28 Install') {{
                 dir('ci') {{
                     git 'https://github.com/quipucords/ci.git'
@@ -186,19 +331,139 @@ stage('Test Install') {{
                     withCredentials([file(credentialsId:
                     '4c692211-c5e1-4354-8e1b-b9d0276c29d9', variable: 'ID_JENKINS_RSA')]) {{
                         withEnv(['DISTRO=f28', 'RELEASE={release}']) {{
-                            setupDocker()
+//                            setupDocker()
 
                             setupQPC()
 
-                            runInstallTests 'f28'
+//                            runInstallTests 'f28'
                         }}
                     }}
                 }}
             }}
-            stage('Fedora 28 Camayoc Tests'){{
-                camayoc_tests()
+//            stage('Fedora 28 Camayoc Tests'){{
+//                camayoc_tests()
+//            }}
+
+            stage('Setup Integration Tests') {{
+                echo 'Fedora 28: Configure Docker'
+                configureDocker()
+                echo 'Fedora 28: Install QPC Client'
+                installQpcClient()
+                echo 'Fedora 28: Setup Scan Users'
+                setupScanUsers()
+                echo 'Fedora 28: Setup Camayoc'
+                setupCamayoc()
             }}
 
+
+        stage('test api') {{
+            echo 'Fedora 28: Test API'
+            startQPCServer()
+            runCamayocTest 'api'
+//            startQPCServer()
+//
+//            sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {{
+//                sh '''
+//                export xdg_config_home=$pwd
+//
+//                set +e
+//                py.test -c pytest.ini -l -ra -s -vvv --junit-xml api-junit.xml --rootdir camayoc/camayoc/tests/qpc camayoc/camayoc/tests/qpc/api
+//                set -e
+//
+//                sudo docker rm $(sudo docker stop $(sudo docker ps -aq))
+//                tar -cvzf test-api-logs.tar.gz log
+//                sudo rm -rf log
+//                '''.stripIndent()
+//            }}
+//            echo 'pre archive artifacts'
+//            archiveArtifacts 'test-api-logs.tar.gz'
+//
+//            echo 'pre api-junit'
+//            junit 'api-junit.xml'
+        }}
+
+
+        stage('Test CLI') {{
+            echo 'Fedora 28: Test CLI'
+            startQPCServer()
+
+            sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {{
+                sh '''\
+                export XDG_CONFIG_HOME=$PWD
+
+                set +e
+                py.test -c pytest.ini -l -ra -vvv --junit-xml cli-junit.xml --rootdir camayoc/camayoc/tests/qpc camayoc/camayoc/tests/qpc/cli
+                set -e
+
+                sudo docker rm $(sudo docker stop $(sudo docker ps -aq))
+                tar -cvzf test-cli-logs.tar.gz log
+                sudo rm -rf log
+                '''.stripIndent()
+            }}
+
+            archiveArtifacts 'test-cli-logs.tar.gz'
+
+            junit 'cli-junit.xml'
+        }}
+
+
+        stage('Test UI Chrome') {{
+            echo 'Fedora 28: Test UI Chrome'
+            startQPCServer()
+
+            sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {{
+                sh 'sudo docker run --net="host" -d -p 4444:4444 -v /dev/shm:/dev/shm:z -v /tmp:/tmp:z selenium/standalone-chrome'
+
+                sleep 3
+
+                sh '''\
+                export XDG_CONFIG_HOME=$PWD
+                export SELENIUM_DRIVER=chrome
+
+                set +e
+                py.test -c pytest.ini -l -ra -vvv --junit-prefix chrome --junit-xml ui-chrome-junit.xml --rootdir camayoc/camayoc/tests/qpc camayoc/camayoc/tests/qpc/ui
+                set -e
+
+                sudo docker rm $(sudo docker stop $(sudo docker ps -aq))
+                tar -cvzf test-ui-chrome-logs.tar.gz log
+                sudo rm -rf log
+                '''.stripIndent()
+            }}
+
+            archiveArtifacts 'test-ui-chrome-logs.tar.gz'
+
+            junit 'ui-chrome-junit.xml'
+        }}
+
+        stage('Test UI Firefox') {{
+            startQPCServer()
+
+            sshagent(['390bdc1f-73c6-457e-81de-9e794478e0e']) {{
+                sh 'sudo docker run --net="host" -d -p 4444:4444 -v /dev/shm:/dev/shm:z -v /tmp:/tmp:z selenium/standalone-firefox'
+
+                sleep 3
+
+                sh '''\
+                export XDG_CONFIG_HOME=$PWD
+                export SELENIUM_DRIVER=firefox
+
+                set +e
+                py.test -c pytest.ini -l -ra -vvv --junit-prefix firefox --junit-xml ui-firefox-junit.xml --rootdir camayoc/camayoc/tests/qpc camayoc/camayoc/tests/qpc/ui
+                set -e
+
+                sudo docker rm $(sudo docker stop $(sudo docker ps -aq))
+                tar -cvzf test-ui-firefox-logs.tar.gz log
+                sudo rm -rf log
+                '''.stripIndent()
+            }}
+
+            archiveArtifacts 'test-ui-firefox-logs.tar.gz'
+
+            junit 'ui-firefox-junit.xml'
+        }}
+
+            runInstallTests 'f28'
+            sh 'ls -lah'
             junit 'junit.xml'
         }}
     }}, 'RHEL6 Install': {{
