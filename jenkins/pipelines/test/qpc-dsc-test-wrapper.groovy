d94f44b171b4569ed6a3695f7f2e01f4fedf4fb8
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2019-12-10 10:03:56 -0500

    added parameter to select which project(s) tests to kick off

--- jenkins/pipelines/test/qpc-dsc-test-wrapper.groovy
+++ jenkins/pipelines/test/qpc-dsc-test-wrapper.groovy
@@ -2,10 +2,13 @@ pipeline {
     agent any
 
     parameters {
+        // General params
+        choice(choices: ['both', 'qpc', 'dsc'], description: "Projects to run", name: 'projects')
         choice(choices: ['rhel8-os'], description: "Node OS", name: 'node_os')
-        //choice(choices: ['branch', 'tag'], description: 'Branch or Tag?', name: 'version_type')
+        // Quipucords Params
         string(defaultValue: '0.9.1', description: "Quipucords Server Version", name: 'qpc_server_install_version')
         string(defaultValue: '0.9.1', description: "QPC CLI Version", name: 'qpc_cli_install_version')
+        // Discovery Params
         string(defaultValue: '0.9.1', description: "Discovery Server Version", name: 'dsc_server_install_version')
         string(defaultValue: '0.9.1', description: "DSC CLI Version", name: 'dsc_cli_install_version')
     }
@@ -13,12 +16,15 @@ pipeline {
     stages {
         stage("Build Info") {
             steps {
-                echo "Node OS: ${params.node_os}\n\nQuipucords Server Version: ${params.qpc_server_install_version}\nQuipucords CLI Version: ${params.qpc_cli_install_version}\n\nDiscovery Server Version: ${params.dsc_server_install_version}\nDiscovery CLI Version: ${params.dsc_cli_install_version}"
+                echo "Projects: ${params.projects}\nNode OS: ${params.node_os}\n\nQuipucords Server Version: ${params.qpc_server_install_version}\nQuipucords CLI Version: ${params.qpc_cli_install_version}\n\nDiscovery Server Version: ${params.dsc_server_install_version}\nDiscovery CLI Version: ${params.dsc_cli_install_version}"
             }
         }
         stage ('Run Tests') {
             parallel {
                 stage ('Test Quipucords') {
+                    when {
+                        expression { params.projects == 'qpc' || params.projects == 'both' }
+                    }//end when
                     steps {
                         build job: 'qpc-dsc-test-pipeline', propagate: false, parameters:[
                             string(name: 'project',value: "qpc"),
@@ -28,9 +34,12 @@ pipeline {
                             string(name: 'server_install_version',value: "${qpc_server_install_version}"),
                             string(name: 'cli_install_version',value: "${qpc_cli_install_version}")
                         ]
-                    }
-                }
+                    }//end steps
+                }//end stage
                 stage ('Test Discovery') {
+                    when {
+                        expression { params.projects == 'dsc' || params.projects == 'both' }
+                    }//end when
                     steps {
                         build job: 'qpc-dsc-test-pipeline2', propagate: false, parameters:[
                             string(name: 'project',value: "dsc"),
